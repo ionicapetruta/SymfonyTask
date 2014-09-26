45fbdb6f1f2253e2b0e012d567501491de1ef883
Ionica <petruta_ionica@yahoo.com>
2014-09-26 14:40:05 +0300

    Create entity for TaskBundle.

---
+++ src/Acme/TaskBundle/Entity/Department.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Acme\TaskBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Department
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Acme\TaskBundle\Entity\DepartmentRepository")
+ */
+class Department
+{
+    /**
+     * @ORM\OneToMany(targetEntity="Employee", mappedBy="departament")
+     */
+    protected $employee;
+
+    public function __construct()
+    {
+        $this->employee = new ArrayCollection();
+    }
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Department
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Add employee.
+     *
+     * @param Employee $employee
+     * @return Department
+     */
+    public function addEmployee(Employee $employee)
+    {
+        $this->employee[] = $employee;
+        return $this;
+    }
+
+    /**
+     * Remove employee.
+     *
+     * @param Employee $employee
+     */
+    public function removeEmployee(Employee $employee)
+    {
+        $this->employee->removeElement($employee);
+    }
+
+    public function getEmployee()
+    {
+        return $this->employee;
+    }
+
+}
---
+++ src/Acme/TaskBundle/Entity/Employee.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Acme\TaskBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Employee
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Acme\TaskBundle\Entity\EmployeeRepository")
+ */
+class Employee
+{
+    /**
+     * @ORM\ManyToOne(targetEntity="Department", inversedBy="employee")
+     * @ORM\JoinColumn(name="department_id", referencedColumnName="id")
+     */
+    protected $department;
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="age", type="integer")
+     */
+    private $age;
+
+    /**
+     * @param mixed $department
+     */
+    public function setDepartment($department)
+    {
+        $this->department = $department;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDepartment()
+    {
+        return $this->department;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Employee
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set age
+     *
+     * @param integer $age
+     * @return Employee
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    /**
+     * Get age
+     *
+     * @return integer 
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+}
